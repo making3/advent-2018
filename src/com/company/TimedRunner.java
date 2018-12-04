9e0fa9b9a406cbbfbf9131d092d87d6762309484
Matthew King <mattking73@gmail.com>
2018-12-03 23:29:36 -0800

    Re-order timedrunner commands

--- src/com/company/TimedRunner.java
+++ src/com/company/TimedRunner.java
@@ -2,10 +2,11 @@ package com.company;
 
 public class TimedRunner {
     public void run(AdventRun adventRun) {
+        Object input = adventRun.getInput();
         final long startTime = System.currentTimeMillis();
-        String result = adventRun.run(adventRun.getInput());
-        System.out.println(adventRun.getRunName() + ": " + result);
+        String result = adventRun.run(input);
         final long endTime = System.currentTimeMillis();
+        System.out.println(adventRun.getRunName() + ": " + result);
         System.out.println(">>>Total execution time: " + (endTime - startTime));
     }
 }
